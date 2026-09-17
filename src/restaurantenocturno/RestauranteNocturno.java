a836ffd21cc7991ffbd60b1da69c92624490e96d
AxlAntunez07 <axl.antunez0770@gmail.com>
2026-09-17 00:53:29 -0600

    Se agrega menu principal y clase Usuario

--- src/restaurantenocturno/RestauranteNocturno.java
+++ src/restaurantenocturno/RestauranteNocturno.java
@@ -4,6 +4,8 @@
  */
 package restaurantenocturno;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -14,8 +16,45 @@ public class RestauranteNocturno {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("RESTAURANTE NOCTURNO");
-        System.out.println("extra:v");
-    }
 
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n==============================");
+            System.out.println("     RESTAURANTE NOCTURNO");
+            System.out.println("==============================");
+            System.out.println("1. Iniciar sesion");
+            System.out.println("2. Registrarse");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opcion: ");
+
+            opcion = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("Inicio de sesion");
+                    break;
+
+                case 2:
+                    System.out.println("Registro de usuario");
+                    break;
+
+                case 3:
+                    System.out.println("Programa finalizado.");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida.");
+            }
+
+        } while (opcion != 3);
+
+        entrada.close();
+    }
 }
+
